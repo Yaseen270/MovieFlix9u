e23285664b14ee39e34c2a2947b0713b7b97c2da
Yaseen270 <bdgamesbazar71@gmail.com>
2025-07-12 23:47:21 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -1075,6 +1075,7 @@ def telegram_webhook():
                     requests.get(f"{TELEGRAM_API_URL}/sendMessage", params={'chat_id': chat_id, 'text': "An unexpected error occurred while processing your request."})
             else:
                 requests.get(f"{TELEGRAM_API_URL}/sendMessage", params={'chat_id': chat_id, 'text': "আমাদের movieflix9u.onrender.com ওয়েবসাইটে আপনাকে স্বাগতম.  আমাদের অপিসিয়াল চ্যানেলে জয়েন করুন @MovieFlix9u  আমাদের অপিসিয়াল গ্রুপে জয়েন করুন @MovieFlix9uChat  আমাদের অপিসিয়াল চ্যানেলে ও গ্রুপে জয়েন করার জন্য আপনাকে ধন্যবাদ."})
+                
     return jsonify(status='ok')
 
 if __name__ == "__main__":
